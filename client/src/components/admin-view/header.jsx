acf949c4da4d49509726901c35560f55f86d45ff
mahekpatel9696 <mahekpatel9696@gmail.com>
2025-09-06 11:02:14 +0530

    Logout Completed (5:05:21)

--- client/src/components/admin-view/header.jsx
+++ client/src/components/admin-view/header.jsx
@@ -1,8 +1,16 @@
 import { Button } from "../ui/button";
 import { AlignJustify, LogOut } from "lucide-react";
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { logoutUser } from "@/store/auth-slice";
 
 function AdminHeader({ setOpen }) {
+  const dispatch = useDispatch();
+
+  function handleLogout() {
+    dispatch(logoutUser());
+  }
+
   return (
     <header className="flex items-center justify-between px-4 py-3 border-b">
       <Button onClick={() => setOpen(true)} className="lg:hidden sm:block">
@@ -10,7 +18,10 @@ function AdminHeader({ setOpen }) {
         <span className="sr-only">Toggle Sidebar</span>
       </Button>
       <div className="flex flex-1 justify-end">
-        <Button className="inline-flex gap-2 items-center rounded-md px-4 py-2 text-sm font-medium shadow">
+        <Button
+          onClick={handleLogout}
+          className="inline-flex gap-2 items-center rounded-md px-4 py-2 text-sm font-medium shadow"
+        >
           <LogOut />
           <span className="sr-only">Log Out</span>
         </Button>
